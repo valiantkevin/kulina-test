e0d3306481733630dc4b0bb858f26b827907b7f1
pepin <valiantkevin@gmail.com>
2018-05-14 21:44:38 +0700

    fibonacci done

---
+++ Algoritma/2.py
@@ -0,0 +1,18 @@
+def fibonacci(n):
+    if n < 0:
+        print("Incorrect input")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a = 0
+        b = 1
+        for x in range(2,n+1):
+            c = a + b
+            a = b
+            b = c
+        return b
+
+n = int(input())
+print(fibonacci(n))
